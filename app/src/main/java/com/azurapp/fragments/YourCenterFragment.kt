af5d2e3ec61f559581264a7ddd3e14ad8c5851e5
adrani_laptop <riadh_adrani@hotmail.fr>
2021-05-13 23:38:08 +0100

    [add] implemented YourCenter fragment

--- app/src/main/java/com/azurapp/fragments/YourCenterFragment.kt
+++ app/src/main/java/com/azurapp/fragments/YourCenterFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import com.azurapp.R
+import com.github.chrisbanes.photoview.PhotoView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class YourCenterFragment : BaseFragment(R.layout.fragment_your_center) {
 
@@ -12,5 +14,24 @@ class YourCenterFragment : BaseFragment(R.layout.fragment_your_center) {
     override fun tag(): String = "YOUR_CENTER_FRAGMENT"
 
     override fun onCreated(view: View, savedInstanceState: Bundle?) {
+
+        safePadding(view)
+
+        val plan : PhotoView = view.findViewById(R.id.fragment_center_plan)
+
+        val levelOne : FloatingActionButton = view.findViewById(R.id.fragment_level_1)
+        levelOne.setOnClickListener {
+            plan.setImageResource(R.drawable.your_center_level_1)
+        }
+
+        val levelZero : FloatingActionButton = view.findViewById(R.id.fragment_level_0)
+        levelZero.setOnClickListener {
+            plan.setImageResource(R.drawable.your_center_level_0)
+        }
+
+        val levelMinusOne : FloatingActionButton = view.findViewById(R.id.fragment_level__1)
+        levelMinusOne.setOnClickListener {
+            plan.setImageResource(R.drawable.your_center_level__1)
+        }
     }
 }
